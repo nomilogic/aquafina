454f26acd6dc0ed45578c80494293e602f3221f6
nomilogic <nomilogic@gmail.com>
2018-09-30 05:06:05 +0500

    s

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if($method == 'POST'){
 	//$response->speech = $speech;
 	//$response->displayText = $speech;
 	$response->source = "webhook";
-	$response->messages =json_decode('[{"type": 2,"platform": "facebook", "title": "What is your Water?","replies": ["Boiled","Filtered"]},{"type": 0,"speech": ""}]');
+	//$response->messages =json_decode('[{"type": 2,"platform": "facebook", "title": "What is your Water?","replies": ["Boiled","Filtered"]},{"type": 0,"speech": ""}]');
 		'
 				echo json_encode($response);
 }
